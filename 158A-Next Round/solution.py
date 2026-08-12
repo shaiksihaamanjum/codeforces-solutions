032fb9ad0d87a4e732c2c2d5b1f0c1f5452c062b
Your GitHub Username <shaiksihaam2245@gmail.com>
2026-08-12 10:01:33 +0530

    Add 158A-Next Round

---
+++ 158A-Next Round/solution.py
@@ -0,0 +1,10 @@
+n,k=map(int,input().split())
+count=0 
+
+scores=list(map(int,input().split()))
+value=scores[k-1]
+for i in scores:
+    if(i>=value and i>0):
+        count+=1 
+print(count)    
+git add "158A-Next Round"
