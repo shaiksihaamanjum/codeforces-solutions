37a81d3b2947038ee4c418002f062fed476e0a21
Your GitHub Username <shaiksihaam2245@gmail.com>
2026-08-21 20:56:08 +0530

    Add 677A-Vanya and Fence

---
+++ 677A-Vanya and Fence/solution.py
@@ -0,0 +1,16 @@
+n,h=map(int,input().split(" "))
+sum=0
+a=map(int,input().split(" "))
+for i in a:
+        if(i<=h):
+           sum+=1
+        else:
+              sum+=2
+print(sum)  
+
+     # 4 2      1 1 8 10 
+    
+# 6 5
+# 7 6 8 9 10 5
+
+    677A-Vanya and Fence
