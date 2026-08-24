ca43aced42c91071a4734bd4c0887545ddec1d36
Your GitHub Username <shaiksihaam2245@gmail.com>
2026-08-24 19:36:43 +0530

    Add 977A-Wrong Subtraction

---
+++ 977A-Wrong Subtraction/solution.py
@@ -0,0 +1,23 @@
+n,k=map(int,input().split())
+for i in range(k):
+    if(n%10==0):
+        n=n//10 
+    else:
+        n=n-1
+print(n)                
+
+
+    
+    
+    
+
+       
+
+
+# 512 4
+# OutputCopy
+# 50
+# InputCopy
+# 1000000000 9
+# OutputCopy
+# 1
